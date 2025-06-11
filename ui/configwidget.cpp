cb25e8cba76221234f4a0d3e11856dae0e199f85
leejkee <leejykee@yeah.net>
2025-06-11 14:05:30 +0800

    feat: create a new widget which config the application

--- ui/configwidget.cpp
+++ ui/configwidget.cpp
@@ -1,13 +1,47 @@
 #include <configwidget.h>
+#include <filepathconfigwidget.h>
+
+#include <QLabel>
+#include <QListWidget>
+#include <QPushButton>
+#include <QStackedWidget>
+#include <QTabWidget>
+#include <memory>
+#include <qlistwidget.h>
+#include <qstackedwidget.h>
 #include <qwidget.h>
+
 namespace Tray::Ui {
 class ConfigWidgetPrivate {
 public:
+    ConfigWidgetPrivate(QWidget* q_ptr);
     enum ConfigType { FilePage, ApperancePage, LyricPage, InvalidPage = -1 };
 
-    QList<QWidget*> m_pageArray;
+    QListWidget* m_menuListWidget;
+    QStackedWidget* m_stackedWidget;
+
+    FilePathConfigWidget* m_filePathConfigWidget;
 };
 
-ConfigWidget::ConfigWidget(QWidget* parent) : QWidget(parent) {}
+ConfigWidgetPrivate::ConfigWidgetPrivate(QWidget *q_ptr)
+{
+    m_menuListWidget = new QListWidget(q_ptr);
+    m_stackedWidget = new QStackedWidget(q_ptr);
+    m_filePathConfigWidget = new FilePathConfigWidget(q_ptr);
+}
+
+ConfigWidget::ConfigWidget(QWidget* parent) : QDialog(parent) {
+
+    d = std::make_unique<ConfigWidgetPrivate>(this);
+    d->m_stackedWidget->addWidget(d->m_filePathConfigWidget);
+
+
+}
+
+void ConfigWidget::initConfigration()
+{
+
+
+}
 
 } // namespace Tray::Ui
--- ui/include/configwidget.h
+++ ui/include/configwidget.h
@@ -1,16 +1,23 @@
 #pragma once
-#include <QWidget>
+#include <QDialog>
 #include <memory>
 
 namespace Tray::Ui {
 
 class ConfigWidgetPrivate;
-class ConfigWidget final : public QWidget {
+class ConfigWidget final : public QDialog {
 public:
     explicit ConfigWidget(QWidget* parent = nullptr);
 
 private:
     std::unique_ptr<ConfigWidgetPrivate> d;
+    
+
+    /// Initialize widget layout and signal/slot connections
+    /// 1. create the layout for the widget
+    /// 2. initialize the areas of the settings
+    /// 3. connect the signals and slots 
+    void initConfigration();
 
 };
 } // namespace Tray::Ui
