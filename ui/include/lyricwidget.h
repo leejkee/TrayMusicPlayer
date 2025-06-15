90c4930c3c3f64075ee24e644d7d017e7063b504
leejkee <leejykee@yeah.net>
2025-06-15 18:56:47 +0800

    feature: create lyricwidget

---
+++ ui/include/lyricwidget.h
@@ -0,0 +1,17 @@
+//
+// Created by cww on 25-6-15.
+//
+#pragma once
+#include <QWidget>
+
+namespace Tray::Ui {
+class LyricWidget : public QWidget{
+  public:
+    explicit LyricWidget(QWidget* parent = nullptr);
+    ~LyricWidget() override;
+
+    enum LyricShowMode {
+
+    };
+};
+}
